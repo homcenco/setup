195dc1a930e67307c91085aefd734c3604abb1bb
Homcenco Vasili <homcenco@gmail.com>
2025-10-22 10:31:11 +0300

    feat(dock): add tiger

--- dock/setup.sh
+++ dock/setup.sh
@@ -5,7 +5,7 @@ set -u
 # APPS array by its name (ex: "App Store".app)
 APPS=("Launchpad"
       "Google Chrome" "Safari"
-      "Discord" "Mail" "Telegram" "Figma"
+      "Discord" "Mail" "Telegram" "Tigerx.com" "Figma"
       "Bruno" "Transmit" "Visual Studio Code" "WebStorm" "Android Studio" "Xcode" "Docker"
       "Termius" "iTerm" "Activity Monitor" "Screenshot"  "Reminders" "Calculator" "System Settings"
 )
